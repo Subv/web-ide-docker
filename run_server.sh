aec7965ba7d428ef03b9c9f1bc06d1601bf6f4d0
Subv <subv2112@gmail.com>
2018-05-29 18:54:02 -0500

    Make www-data own the c9 .sessions folder and use sudo

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-docker-php-entrypoint apache2-foreground & su -c /opt/c9/bin/cloud9.sh -l 0.0.0.0 -p 3131 -w /var/www/html/ www-data
+docker-php-entrypoint apache2-foreground &
+sudo -u www-data /opt/c9/bin/cloud9.sh -l 0.0.0.0 -p 3131 -w /var/www/html/
